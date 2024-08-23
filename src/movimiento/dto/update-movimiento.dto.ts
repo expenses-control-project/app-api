f02cdb8b950317f230330f4aeb4a8ecd92102efd
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-08-22 22:06:27 -0300

    correccion de carpetas del proyecto

--- src/movimiento/dto/update-movimiento.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMovimientoDto } from './create-movimiento.dto';
-
-export class UpdateMovimientoDto extends PartialType(CreateMovimientoDto) {}
