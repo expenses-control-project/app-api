9126b4c2db8debfa5fb192358f2d708b271edc76
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-08-22 22:11:07 -0300

    correccion en archivo config

--- src/config/configFile.ts
+++ src/config/configFile.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({path: '../.env'});
+dotenv.config({path: '../../.env'});
 
 export const DATABASE_URL = process.env.DATABASE_URL as string;
 export const PORT = process.env.PORT || 3002;
