acbccb0fad577c7ddb272433c79e1525a9da788c
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-09-30 21:07:21 -0300

    Configuracion de cors para el frontend

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import {AppModule} from './app.module';
 import {PORT} from './config/configFile';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {ValidationPipe} from '@nestjs/common';
+import {CorsOptions} from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api');
 
-	app.enableCors();
+	const corsOptions: CorsOptions = {
+		origin: ['http://localhost:5173'],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	};
+
+	app.enableCors(corsOptions);
 
 	app.useGlobalPipes(
 		new ValidationPipe({
