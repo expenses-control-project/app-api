98f2b6edc2fd3c5183989a602afac209fa46b10e
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-10-22 09:05:10 -0300

    Cors front actualizado

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,44 @@
-import {NestFactory, Reflector} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {PORT} from './config/configFile';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import {ClassSerializerInterceptor, ValidationPipe} from '@nestjs/common';
-import {CorsOptions} from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PORT } from './config/configFile';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
-	const corsOptions: CorsOptions = {
-		origin: ['http://localhost:5173'],
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		credentials: true,
-	};
+  const corsOptions: CorsOptions = {
+    origin: ['https://app-expenses-control.web.app/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
 
-	app.enableCors(corsOptions);
+  app.enableCors(corsOptions);
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transformOptions: {
-				enableImplicitConversion: true,
-			},
-		}),
-	);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
-	const reflector = app.get(Reflector);
+  const reflector = app.get(Reflector);
 
-	app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
-	const config = new DocumentBuilder()
-		.setTitle('Expenses-control')
-		.setDescription('Documentacion de la API Expenses-control')
-		.setVersion('1.0.0')
-		.build();
+  const config = new DocumentBuilder()
+    .setTitle('Expenses-control')
+    .setDescription('Documentacion de la API Expenses-control')
+    .setVersion('1.0.0')
+    .build();
 
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
-	await app.listen(PORT);
+  await app.listen(PORT);
 }
 bootstrap();
