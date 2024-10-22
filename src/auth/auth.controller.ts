ccacc2cf820a33275429e7cb64dba8234d923694
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-10-22 19:14:27 -0300

    Public acces Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,22 @@
-import {Body, Controller, Post, UnauthorizedException} from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Post,
+	UnauthorizedException,
+	UseGuards,
+} from '@nestjs/common';
 import {AuthService} from './auth.service';
 import {AuthDTO} from './auth.dto';
+import {PublicAcces} from './decorators/public.decorator';
+import {AuthGuard} from './guards/auth.guard';
+import {RolesGuard} from './guards/roles.guard';
 
 @Controller('auth')
+@UseGuards(AuthGuard, RolesGuard)
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
+	@PublicAcces()
 	@Post('login')
 	async login(@Body() {usuario, password}: AuthDTO): Promise<any> {
 		const userValidate = await this.authService.validarUsuario(
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import {AuthService} from './auth.service';
 import {AuthController} from './auth.controller';
 import {UsuarioService} from 'src/usuario/usuario.service';
 import {UsuarioModule} from 'src/usuario/usuario.module';
-import {Prisma} from '@prisma/client';
 import {PrismaModule} from 'src/config/prisma.module';
 
 @Global()
